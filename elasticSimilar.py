adc4a6869d58a3eaadab270e116d69bd77b9e79a
thanos <thanos.karousos@gmail.com>
2020-11-11 23:50:19 +0200

    now shows the whole document as the result

--- elasticSimilar.py
+++ elasticSimilar.py
@@ -4,20 +4,31 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
   [{'host': 'localhost', 'port': 9200}])
-print(es)
 
 searchTerm = input("What do you want to search for? ")
 
 query_body = {
   "query": {
       "match": {
-          "title": searchTerm
+          "title": searchTerm #isws 8elei more_like
       }
   }
 }
 
-es.search(index="movie_index1", body=query_body)
-
 #to size einai epeidi by default epistrefei mono 10 items sto search
 result = es.search(index="movie_index1", body=query_body, size=999)
-print ("total hits:", len(result["hits"]["hits"]))
+
+print("Got %d Hits:" % result['hits']['total']['value'])
+#or: print("Got " + str(result['hits']['total']['value']) + " Hits:")
+
+
+for hit in result['hits']['hits']:
+    print(hit["_source"])
+
+
+##################################
+
+#pros to paron emfanizei apotelesma mono otan yparxei 
+#exact match tou query me ton titlo
+#px: An query="gin", den 8a emfanisei to "Origins" sta apotelesmata
+#paroti to gin emperiexetai sto Origins
