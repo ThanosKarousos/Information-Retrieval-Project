d4c1f026b93ce6f36cc2f894bb6f80b0b8df7bb6
thanos <thanos.karousos@gmail.com>
2020-12-11 13:59:03 +0200

    Completed dataframe

--- elasticSimilar.py
+++ elasticSimilar.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from elasticsearch import Elasticsearch
+from pandas.core.frame import DataFrame
 from pandasticsearch import Select
 import pandas as pd
 
@@ -20,7 +21,7 @@ es = Elasticsearch(
     [{'host': 'localhost', 'port': 9200}])
 
 searchTerm = input("What do you want to search for? ")
-#userIDsearch = input("What is your userId? ")
+userIDsearch = input("What is your userId? ")
 
 # to size einai epeidi by default epistrefei mono 10 items sto search
 result = es.search(index="movie_index1",
@@ -44,6 +45,7 @@ genres = pandas_df['genres']
 movieIds = pandas_df['movieId']
 scores = pandas_df['_score']
 
+
 ratings_list = []
 for i in range(movieIds.size):
 
@@ -66,18 +68,64 @@ for i in range(movieIds.size):
     ratingAVG = es.search(index="ratings_index1", body=queryBody1, size=999)
 
     movieRatingAVG = ratingAVG["aggregations"]["avg_movie_rating"]["value"]
-    #r = {"movieID": movieIds[i], "avgRating": movieRatingAVG} ---mporei na xreiastei etsi
-    #ratings_list.append(r)
+    # r = {"movieID": movieIds[i], "avgRating": movieRatingAVG} ---mporei na xreiastei etsi
+    # ratings_list.append(r)
     ratings_list.append(movieRatingAVG)
 
-rating_df = pd.DataFrame(ratings_list, columns = ['AvgRating'])
+rating_df = pd.DataFrame(ratings_list, columns=['AvgRating'])
 
-movieDataframe = pd.concat(
-    [movieIds, titles, genres, scores, rating_df], axis=1, sort=False, join='outer')
-print(movieDataframe)
+#movieDataframe = pd.concat(
+#    [movieIds, titles, genres, scores, rating_df], axis=1, sort=False, join='outer')
+#print(movieDataframe)
 ##################################
 
 # pros to paron emfanizei apotelesma mono otan yparxei
 # exact match tou query me ton titlo
 # px: An query="gin", den 8a emfanisei to "Origins" sta apotelesmata
 # paroti to gin emperiexetai sto Origins
+userRatings = []
+
+
+for i in range(movieIds.size):
+
+    query2 = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "match": {
+                            "userId": userIDsearch
+                        }
+                    },
+                    {
+                        "match": {
+                            "movieId": movieIds[i]
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    test = es.search(index="ratings_index1", body=query2, size=999)
+    
+    appendedRating = 0
+
+
+    if(len(test["hits"]["hits"]) != 0):
+
+        for num in (test['hits']['hits']):
+            appendedRating = num['_source']['rating']
+
+
+    userRatings.append(appendedRating)
+
+
+userRatingsDF = DataFrame(userRatings, columns=['User_Rating'])
+
+movieDataframe = pd.concat(
+    [movieIds, titles, genres, scores, rating_df, userRatingsDF], axis=1, sort=False, join='outer')
+print(movieDataframe)
+
+# metric = 1.1*BM25 + 1.3*AvgRating + 1.5*userRating
+# test: userID = 187, search = sex
